199fbb7ebd808f2dc13e2593e3de9d0e942c61d8
Arman Abdrakhmanov <arma64n@gmail.com>
2020-01-15 23:55:53 +0600

    initial commit

---
+++ src/components/Format.vue
@@ -0,0 +1,29 @@
+<template>
+    <img :src="require(`@/assets/format/${icon}.svg`)" :alt="icon">
+</template>
+
+<script>
+export default {
+    props: {
+        format: String
+    },
+    computed: {
+        icon () {
+            switch (this.format) {
+                case 'pdf':
+                    return 'pdf'
+                case 'jpg':
+                case 'jpeg':
+                case 'png':
+                    return 'image'
+                case 'xls':
+                case 'xlsx':
+                    return 'excel'
+                case 'doc':
+                case 'docx':
+                    return 'word'
+            }
+        }
+    }
+}
+</script>
