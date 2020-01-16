fd533f49beb75216ffd85bf5d5be67615e7ca1a0
arma64n <arma64n@gmail.com>
2020-01-16 10:11:35 +0600

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,22 @@
-import Vue from "vue"
 import Format from "./Format.vue"
 import Dots from "./Dots.vue"
 
-// export default {
-//     install(Vue, options) {
-//      // Let's register our component globally
-//      // https://vuejs.org/v2/guide/components-registration.html
-//         Vue.component("ui-format", Format);
-//         Vue.component("ui-dots", Dots);
-//     }
-// };
+export default {
+    install(Vue) {
+     // Let's register our component globally
+     // https://vuejs.org/v2/guide/components-registration.html
+        Vue.component("ui-format", Format);
+        Vue.component("ui-dots", Dots);
+    }
+};
 
-const Components = {
-    Format,
-    Dots
-}
+// const Components = {
+//     Format,
+//     Dots
+// }
 
-Object.keys(Components).forEach(name => {
-    Vue.component(`ui-${name}`, Components[name])
-})
+// Object.keys(Components).forEach(name => {
+//     Vue.component(`ui-${name}`, Components[name])
+// })
 
-export default Components
+// export default Components
