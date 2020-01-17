2949a2551d67dab1e45508d4e511aa0b69a7927a
arma64n <arma64n@gmail.com>
2020-01-17 16:24:36 +0600

    fix

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,19 @@
 import Format from './components/dumb/Format'
 import Dots from './components/dumb/Dots'
 
-const dumb = {
-    Format,
-    Dots
-}
+// const dumb = {
+//     Format,
+//     Dots
+// }
 
 const ComponentLibrary = {
     install(Vue) {
-        Object.keys(dumb).forEach(item => {
-            Vue.component(`dumb-${item.toLowerCase()}`, dumb[item])
-        })
+        Vue.component(`dumb-format`, Format)
+        Vue.component(`dumb-dots`, Dots)
+
+        // Object.keys(dumb).forEach(item => {
+        //     Vue.component(`dumb-${item.toLowerCase()}`, dumb[item])
+        // })
     }
 }
 
