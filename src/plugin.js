a6173ae21ade1b9ed1fd062dca6b12480b5ddaac
arma64n <arma64n@gmail.com>
2020-01-28 11:59:01 +0600

    dynamic import

--- src/plugin.js
+++ src/plugin.js
@@ -1,19 +1,32 @@
-import DumbFormat from './components/dumb/Format'
-import DumbDots from './components/dumb/Dots'
-import DumbActions from './components/dumb/Actions'
+// import DumbFormat from './components/dumb/Format'
+// import DumbDots from './components/dumb/Dots'
+// import DumbActions from './components/dumb/Actions'
 import './assets/styles/ui.scss'
 
-const Dumb = {
-    DumbFormat,
-    DumbDots,
-    DumbActions
-}
+// const Dumb = {
+//     DumbFormat,
+//     DumbDots,
+//     DumbActions
+// }
+
+const dumbComponents = require.context( './components/dumb', false, /\w+\.vue$/ )
+const smartComponents = require.context( './components/smart', false, /\w+\.vue$/ )
 
 const ComponentLibrary = {
     install(Vue) {
-        Object.keys(Dumb).forEach(name => {
-            Vue.component(name, Dumb[name])
+        dumbComponents.keys().forEach(fileName => {
+            const componentConfig = dumbComponents(fileName)
+            Vue.component(`dumb-${componentConfig.default}`, componentConfig.default)
         })
+
+        smartComponents.keys().forEach(fileName => {
+            const componentConfig = smartComponents(fileName)
+            Vue.component(`smart-${componentConfig.default}`, componentConfig.default)
+        })
+
+        // Object.keys(Dumb).forEach(name => {
+        //     Vue.component(name, Dumb[name])
+        // })
     }
 }
 
